6a23c1bfe03ef6f4d124acce54415350811ecf8e
James Lester <james@jameslester.xyz>
2025-11-17 12:57:06 +0000

    3. Better lighting

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -8,9 +8,24 @@ import Pitch from "@/components/Pitch";
 
 const Scene: FC = () => {
   return (
-    <Canvas className="bg-sky-200" camera={{ position: [0, 0.5, 7], fov: 70 }}>
+    <Canvas
+      className="bg-sky-200"
+      camera={{ position: [0, 0.5, 7], fov: 70 }}
+      shadows
+    >
+      {/* Lights */}
       <ambientLight />
+      <directionalLight
+        position={[5, 10, 7.5]}
+        intensity={1.5}
+        castShadow
+        shadow-camera-right={15}
+        shadow-camera-top={15}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+      />
 
+      {/* Scene Objects */}
       <group position={[0, -1, 0]}>
         <Pitch />
         <Goal />
